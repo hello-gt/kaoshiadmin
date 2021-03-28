927a8ee2661fff724d5f758be8372d1e486743aa
hello-gt <hello_lgt@163.com>
2021-03-28 22:49:50 +0800

    前端页面

---
+++ application/index/controller/TempArticle.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class TempArticle extends Controller
+{
+    public function udai_article1()
+    {
+        return $this->fetch();
+    }
+}
